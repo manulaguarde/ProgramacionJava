2f90e1416c614bd22d6ed6e52fbefe377299f2a9
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-10-27 19:19:30 +0100

    ejercicio a de figuras completo

--- programacion/src/ud3/Hoja3EjercicioFiguras.java
+++ programacion/src/ud3/Hoja3EjercicioFiguras.java
@@ -10,13 +10,35 @@ public class Hoja3EjercicioFiguras {
 
 	public static void main(String[] args) {
 		Scanner entrada=new Scanner(System.in);
-		int i=0,j=0;
+		int i=0,j=0,numFilas, cont=0, k=0,l=0 ,contDos;
+		
 		
 		System.out.println("ingrese un número de filas");
+		numFilas=entrada.nextInt();
 		
-		for(i=0;i<5;i++) {
-			
-		}
+		/*for(i=0;i<numFilas;i++) {
+			for(j=0;j<=cont;j++) {
+			System.out.print("*");	
+			}
+			cont++;
+			System.out.println();
+			}*/
+		k=numFilas-1;
+		
+		for(i=0;i<numFilas;i++) {
+			for(j=0;j<=cont;j++) {
+
+				for(l=k;l>0;l--) {
+					System.out.print(" ");
+					
+				}
+				k--;
+				System.out.print("*");
+				
+			}
+			cont=cont+2;
+			System.out.println();
+		}	
 
 	}
 
