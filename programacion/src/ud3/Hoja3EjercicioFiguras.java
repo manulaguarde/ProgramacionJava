8117898f851108e6bc286e63a862e5d6435b0f7e
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-10-27 14:17:56 +0100

    agrego ejercicio para hacer

---
+++ programacion/src/ud3/Hoja3EjercicioFiguras.java
@@ -0,0 +1,23 @@
+package ud3;
+
+import java.util.Scanner;
+
+/*
+ * Realiza un programa usando bucles que muestren las siguientes figuras. Se deben pedir por
+teclado el número de filas de la figura antes de dibujarlas.
+*/
+public class Hoja3EjercicioFiguras {
+
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		int i=0,j=0;
+		
+		System.out.println("ingrese un número de filas");
+		
+		for(i=0;i<5;i++) {
+			
+		}
+
+	}
+
+}
