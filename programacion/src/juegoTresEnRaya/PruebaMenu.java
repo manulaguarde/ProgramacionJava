4b7e6fe6c044dbdd349a31511eb72df982065a28
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-11-15 20:55:48 +0100

    Continuo juego de tres en raya

--- programacion/src/juegoTresEnRaya/PruebaMenu.java
+++ programacion/src/juegoTresEnRaya/PruebaMenu.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class PruebaMenu {
 	static Scanner entrada=new Scanner(System.in);
 	static char[] tablero=new char[9];
+	static int victoriasJugador1=0;
+	static int victoriasJugador2=0;
+	static int empates=0;
 	public static void main(String[] args) {
 
-		int j=1;
 		int opcion;
+
 		
 		do {
 			System.out.println("1. Jugar una partida\n"
@@ -17,16 +20,54 @@ public class PruebaMenu {
 					+ "Seleccione una opción:");
 			opcion=entrada.nextInt();
 			
+			int i=0;
 			switch (opcion){
 				case 1:
 					dibujaTablero();
-					mueveJugador1();
-					mueveJugador2();
-					mueveJugador1();
-					mueveJugador2();
+					
+					do{
+						System.out.println("Introduzca movimiento (Jugador 1)");
+						int movimiento1 = entrada.nextInt();
 
+						while(movimientoValido(movimiento1)==false) {
+							System.out.println("Movimiento no válido. Jugador 1: vuelve a ingresar otro movimiento");
+							movimiento1=entrada.nextInt();
+						}
+						mueveJugador1(movimiento1);
+						if(ganaJugador1()) {
+							System.out.println("Ha ganado el jugador 1!");
+							victoriasJugador1+=1;
+							break;
+						}
+						i++;
+						if(i>=9) {
+							System.out.println("Empate");
+							empates+=1;
+						}else {
+							System.out.println("Introduzca movimiento (Jugador 2)");
+							int movimiento2 = entrada.nextInt();
+							while(movimientoValido(movimiento2)==false) {
+								System.out.println("Movimiento no válido. Jugador 2: vuelve a ingresar otro movimiento");
+								movimiento2=entrada.nextInt();
+							}
+							mueveJugador2(movimiento2);
+						}
+						if(ganaJugador2()) {
+							System.out.println("Ha ganado el jugador 2!");
+							victoriasJugador2+=1;
+							break;
+						}
+						i++;
+					}while(i<9);
+					
+					iniciar();
+					
 					break;
 				case 2:
+					System.out.println("Estadísticas:\n"
+									+ "El Jugador 1 ha ganado "+victoriasJugador1+" veces\n"
+									+ "El jugador 2 ha ganado "+victoriasJugador2+" veces\n"
+									+ "Hubieron "+empates+" partidas que acabaron en empate");
 					break;
 				case 3: 
 					System.out.println("Has salido");
@@ -39,108 +80,164 @@ public class PruebaMenu {
 		}while(opcion!=3);
 
 	}
+	// Tablero de 3x3, 9 casilleros 
+	// 7 filas x13 columas en espacio de código
 	public static void dibujaTablero(){
-		int j=1;
-		for(int i=1;i<=7;i++) {
-			for(int raya=1;raya<=13;raya++) {
-				if(i%2!=0)
-				System.out.print("-");
-			}
-			for(int barra=1; barra<4; barra++) {
-				if(i%2==0) {
-					System.out.print("|");
-					for(int espacio=1;espacio==1;espacio++) {
+		// Iniciación variable del contenido
+		int i=1;
+		// Bucle que controla las filas del tablero
+		for(int filas=1;filas<=7;filas++) {
+			 // Filas impares
+			if(filas%2!=0)
+				System.out.print("-------------");
+			// Filas pares
+			if(filas%2==0) {
+				// Bucle que controla las columnas (13 espacios por fila)
+				for(int columna=1; columna<=13; columna++) {
+					// 4 barras verticales (laterales de los casilleros)
+					if(columna==1||columna==5||columna==9||columna==13)
+						System.out.print("|");
+					// Espacios sin contenido (que simepre son pares)
+					if(columna%2==0)
 						System.out.print(" ");
-						for(int numero=1;numero==1;numero++){
-							System.out.print(j);
-						}
-						j++;
-						System.out.print(" ");
+					// Contenido del casillero (del 1 al 9)
+					if(columna==3||columna==7||columna==11) {
+						System.out.print(i);
+						// Se incrementa el contenido para la siguiente casilla 
+						i++;
 					}
 				}
 			}
-			if(i%2!=0) {
-				System.out.println();
-			}else {
-				System.out.println("|");
-			}
+			System.out.println();
 		}
 	}
 	
-	public static void mueveJugador1() {
-		System.out.println("Ingrese un movimiento");
-		int movimiento = entrada.nextInt();
-		int j=0;
-		for (int i = 1; i <= 7; i++) {
-			for (int raya = 1; raya <= 13; raya++) {
-				if (i % 2 != 0)
-					System.out.print("-");
-			}
-			for (int barra = 1; barra < 4; barra++) {
-				if (i % 2 == 0) {
-					System.out.print("|");
-					for (int espacio = 1; espacio == 1; espacio++) {
+	public static void mueveJugador1(int pos) {
+		int i=0;
+		for (int filas = 1; filas <= 7; filas++) {
+				if (filas % 2 != 0)
+					System.out.print("-------------");
+			if (filas % 2 == 0) { 		
+				for (int columna = 1; columna <= 13; columna++) {
+					if(columna==1||columna==5||columna==9||columna==13) 
+						System.out.print("|");
+					if(columna%2==0) 
 						System.out.print(" ");
-						for (int numero = 1; numero == 1; numero++) {
-							if (j == movimiento - 1) {
-								tablero[j] = 'X';
-								System.out.print(tablero[j]);
-							/*} else if ((j == movimiento - 1) && ((j + 1) % 2 == 0)) {
-								tablero[j] = 'O';
-								System.out.print(tablero[j]);*/
-							} else {
-								System.out.print(tablero[j]);
-							}
+					if(columna==3||columna==7||columna==11) { 
+						if (i == pos - 1) { 
+							tablero[i] = 'X'; 
+							System.out.print(tablero[i]);
+						}else { //
+								System.out.print(tablero[i]); 
 						}
-						j++;
-						System.out.print(" ");
+						i++;
 					}
 				}
 			}
-			if (i % 2 != 0) {
-				System.out.println();
-			} else {
-				System.out.println("|");
-			}
+			System.out.println();
 		}
 		
 	}
-	public static void mueveJugador2() {
-		int j=0;
-		System.out.println("Ingrese un movimiento");
-		int movimiento = entrada.nextInt();
-		for (int i = 1; i <= 7; i++) {
-			for (int raya = 1; raya <= 13; raya++) {
-				if (i % 2 != 0)
-					System.out.print("-");
-			}
-			for (int barra = 1; barra < 4; barra++) {
-				if (i % 2 == 0) {
-					System.out.print("|");
-					for (int espacio = 1; espacio == 1; espacio++) {
+	public static void mueveJugador2(int pos) {
+		int i=0;
+		for (int filas = 1; filas <= 7; filas++) {
+				if (filas % 2 != 0)
+					System.out.print("-------------"); 
+			if (filas % 2 == 0) { 	
+				for (int columna = 1; columna <= 13; columna++) {
+					if(columna==1||columna==5||columna==9||columna==13) 
+						System.out.print("|");
+					if(columna%2==0) 
 						System.out.print(" ");
-						for (int numero = 1; numero == 1; numero++) {
-							/*if (j == movimiento - 1) {
-								tablero[j] = 'X';
-								System.out.print(tablero[j]);
-							}*/if (j == movimiento - 1) {
-								tablero[j] = 'O';
-								System.out.print(tablero[j]);
-							} else {
-								System.out.print(tablero[j]);
-							}
+					if(columna==3||columna==7||columna==11) {  
+						if (i == pos - 1) { 
+							tablero[i] = 'O'; 
+							System.out.print(tablero[i]);
+						}else { //
+								System.out.print(tablero[i]);
 						}
-						j++;
-						System.out.print(" ");
+						i++;
 					}
 				}
 			}
-			if (i % 2 != 0) {
-				System.out.println();
-			} else {
-				System.out.println("|");
-			}
+			System.out.println();
 		}
 	}
-
+	
+	public static boolean ganaJugador1() {
+		// Victorias horizontales
+		if (tablero[0]=='X' && tablero[1]=='X' && tablero[2]=='X') {
+			return true;
+		}
+		if (tablero[3]=='X' && tablero[4]=='X' && tablero[5]=='X') {
+			return true;
+		}
+		if (tablero[6]=='X' && tablero[7]=='X' && tablero[8]=='X') {
+			return true;
+		}
+		// Victorias verticales
+		if (tablero[0]=='X' && tablero[3]=='X' && tablero[6]=='X') {
+			return true;
+		}
+		if (tablero[1]=='X' && tablero[4]=='X' && tablero[7]=='X') {
+			return true;
+		}
+		if (tablero[2]=='X' && tablero[5]=='X' && tablero[8]=='X') {
+			return true;
+		}
+		// Victorias en diagonal
+		if (tablero[0]=='X' && tablero[4]=='X' && tablero[8]=='X') {
+			return true;
+		}
+		if (tablero[2]=='X' && tablero[4]=='X' && tablero[6]=='X') {
+			return true;
+		}
+		
+		return false;
+		
+	}
+	public static boolean ganaJugador2() {
+		// Victorias horizontales
+		if (tablero[0]=='O' && tablero[1]=='O' && tablero[2]=='O') {
+			return true;
+		}
+		if (tablero[3]=='O' && tablero[4]=='O' && tablero[5]=='O') {
+			return true;
+		}
+		if (tablero[6]=='O' && tablero[7]=='O' && tablero[8]=='O') {
+			return true;
+		}
+		// Victorias verticales
+		if (tablero[0]=='O' && tablero[3]=='O' && tablero[6]=='O') {
+			return true;
+		}
+		if (tablero[1]=='O' && tablero[4]=='O' && tablero[7]=='O') {
+			return true;
+		}
+		if (tablero[2]=='O' && tablero[5]=='O' && tablero[8]=='O') {
+			return true;
+		}
+		// Victorias en diagonal
+		if (tablero[0]=='O' && tablero[4]=='O' && tablero[8]=='O') {
+			return true;
+		}
+		if (tablero[2]=='O' && tablero[4]=='O' && tablero[6]=='O') {
+			return true ;
+		}
+		
+		return false;
+	}
+	public static void iniciar() {
+		
+		for(int i=0;i<9;i++) tablero[i]=' ';
+	}
+	public static boolean movimientoValido(int pos) {
+		
+		if ((pos>=1 || pos<=9) && (tablero[pos-1]!='X' && tablero[pos-1]!='O')){
+			
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
--- programacion/src/juegoTresEnRaya/PruebaTableroVacio.java
+++ programacion/src/juegoTresEnRaya/PruebaTableroVacio.java
@@ -9,26 +9,51 @@ public class PruebaTableroVacio {
 
 	public static void main(String[] args) {
 		int j = 0;
+		System.out.println("Introduzca movimiento (Jugador 1)");
+		int movimiento1 = entrada.nextInt();
+		mueveJugador1(movimiento1);
 
-		System.out.println("Ingrese un movimiento");
-		int movimiento = entrada.nextInt();
+		System.out.println("Introduzca movimiento (Jugador 2)");
+		int movimiento2 = entrada.nextInt();
+		mueveJugador2(movimiento2);
+		
 		for (int i = 1; i <= 7; i++) {
-			for (int raya = 1; raya <= 13; raya++) {
-				if (i % 2 != 0)
-					System.out.print("-");
+			if (i % 2 != 0)
+				System.out.print("-------------");
+			if (i % 2 == 0) {
+				for (int columnas = 1; columnas <= 13; columnas++) {
+					if (columnas==1 || columnas==5 || columnas==9 || columnas==13) {
+						System.out.print("|");
+					}
+					if (columnas%2==0) {
+						System.out.print(" ");
+					}
+					if (columnas==3 || columnas==7 || columnas==11){
+							tablero[j]=' ';
+							System.out.print(tablero[j]);
+							
+					}
+				}
 			}
-			for (int barra = 1; barra < 4; barra++) {
-				if (i % 2 == 0) {
+			System.out.println();
+
+	}
+
+}
+	public static void mueveJugador1(int pos) {
+		int j=0;
+		for (int i = 1; i <= 7; i++) { //i son las filas del tablero
+				if (i % 2 != 0)
+					System.out.print("-------------"); //dibuja las rayas solo en las filas impares
+			if (i % 2 == 0) { 		//En las filas pares es donde hace una barra verticales, espacios y rellena el valor
+				for (int barra = 1; barra < 4; barra++) {
 					System.out.print("|");
 					for (int espacio = 1; espacio == 1; espacio++) {
 						System.out.print(" ");
 						for (int numero = 1; numero == 1; numero++) {
-							if (j == movimiento - 1) {
+							if (j == pos - 1) {
 								tablero[j] = 'X';
 								System.out.print(tablero[j]);
-							/*} else if ((j == movimiento - 1) && ((j + 1) % 2 == 0)) {
-								tablero[j] = 'O';
-								System.out.print(tablero[j]);*/
 							} else {
 								System.out.print(tablero[j]);
 							}
@@ -44,7 +69,36 @@ public class PruebaTableroVacio {
 				System.out.println("|");
 			}
 		}
-
+		
 	}
-
+	public static void mueveJugador2(int pos) {
+		int j=0;
+		for (int i = 1; i <= 7; i++) {
+				if (i % 2 != 0)
+					System.out.print("-------------");
+			if (i % 2 == 0) {
+				for (int barra = 1; barra < 4; barra++) {
+					System.out.print("|");
+					for (int espacio = 1; espacio == 1; espacio++) {
+						System.out.print(" ");
+						for (int numero = 1; numero == 1; numero++) {
+							if (j == pos - 1) {
+								tablero[j] = 'O';
+								System.out.print(tablero[j]);
+							} else {
+								System.out.print(tablero[j]);
+							}
+						}
+						j++;
+						System.out.print(" ");
+					}
+				}
+			}
+			if (i % 2 != 0) {
+				System.out.println();
+			} else {
+				System.out.println("|");
+			}
+		}
+	}
 }
