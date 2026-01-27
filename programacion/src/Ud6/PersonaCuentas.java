60958aa551c0dcfc4e1dd2cf5cc71bf674824417
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-01-27 22:43:01 +0100

    Empiezo ejercicio de POO nuevo

--- programacion/src/Ud6/PersonaCuentas.java
+++ programacion/src/Ud6/PersonaCuentas.java
@@ -1,5 +1,51 @@
 package Ud6;
 
 public class PersonaCuentas {
+	
+	private int cont=0;
+	private String dni;
+	private CuentaPersona[] cuenta;
+	
+	public PersonaCuentas (String dni) {
+		this.dni=dni;
+	}
 
+	public boolean añadirCuenta(CuentaPersona[] cuenta) {
+		if(cont<3){
+			this.cuenta[cont]=cuenta[cont];
+			cont++;
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public boolean esMoroso() {
+		for(int i=0;i<cuenta.length;i++) {
+			if(cuenta[i].consultaSaldo()<0) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/*public double mostrarSaldo(String numCuenta) {
+		
+		for(int j=0;j<cuenta.length;j++) {
+			return cuenta[j].consultaSaldo();
+		}
+		
+	}*/
+	
+	public CuentaPersona[] dameCuenta(String numCuenta) {
+		
+			return cuenta;
+		
+	}
+	
+	/*public String toString() {
+		for(int i=0;i<3;i++) {
+			return "DNI: "+this.dni+" número de cuenta: "+cuenta[i].toString();
+		}
+	}*/
 }
