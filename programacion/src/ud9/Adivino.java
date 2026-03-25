25d4dcf49c38ad4554ea3f396a8518c36d90c68f
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-03-26 00:17:30 +0100

    ejercicio de la clase

--- programacion/src/ud9/Adivino.java
+++ programacion/src/ud9/Adivino.java
@@ -13,6 +13,7 @@ etiquetas para que quede mejor si se estima necesario.
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -21,31 +22,67 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Adivino extends JFrame implements ActionListener {
-	
+	Random genAle=new Random();
 	private JPanel panel;
 	private JLabel piensaNum, tuNumEs;
 	private JTextField campoNumAdivinado;
-	private JButton mayor,menor,acertaste;
+	private JButton empieza,mayor,menor,acertaste;
+	private int max,min,num;
 	
 	public Adivino() {
 		panel=new JPanel();
-		piensaNum=new JLabel();
-		tuNumEs=new JLabel();
-		campoNumAdivinado=new JTextField();
-		mayor=new JButton();
-		menor= new JButton();
-		acertaste=new JButton();
+		piensaNum=new JLabel("Piensa un número del 1 al 100");
+		//tuNumEs=new JLabel("Tu número es");
+		campoNumAdivinado=new JTextField(30);
+		mayor=new JButton("Es Mayor");
+		menor= new JButton("Es Menor");
+		acertaste=new JButton("Has acertado");
+		empieza=new JButton("Has click aqui cuando estés listo");
+		max=100;
+		min=0;
+		num=genAle.nextInt(101);
 		
 		this.add(panel);
+		panel.add(piensaNum);
+		panel.add(empieza);
+		panel.add(campoNumAdivinado);
+		panel.add(mayor);
+		panel.add(menor);
+		panel.add(acertaste);
+		
+		
+		empieza.addActionListener(this);
+		mayor.addActionListener(this);
+		menor.addActionListener(this);
+		acertaste.addActionListener(this);
 		
 		
 		this.setSize(300, 200);
+		this.setLocation(200, 200);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		this.setVisible(true);
+		
 		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getSource()==empieza)
+			campoNumAdivinado.setText("Tu número es: "+num+"?");
+		if(e.getSource()==mayor) {
+			min=num;
+			num=genAle.nextInt(max-min)+min;
+			campoNumAdivinado.setText("Tu número es: "+num+"?");
+		}
+		if(e.getSource()==menor) {
+			max=num;
+			num=genAle.nextInt(max-min)+min;
+			campoNumAdivinado.setText("Tu número es: "+num+"?");
+		}
+		if(e.getSource()==acertaste) {
+			campoNumAdivinado.setText("Acerte!");
+		}
 		
 	}
 
---
+++ programacion/src/ud9/UsoAdivino.java
@@ -0,0 +1,10 @@
+package ud9;
+
+public class UsoAdivino {
+
+	public static void main(String[] args) {
+		Adivino v1= new Adivino();
+
+	}
+
+}
